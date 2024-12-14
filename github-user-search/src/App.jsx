69c7cd92ea3c24aeb45a698a481b2913891c6112
Damaris Irungu <damarisirungu98@gmail.com>
2024-12-14 14:24:35 +0300

    Prepared the Application Structure

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserSearch from './components/UserSearch';
 
-function App() {
+function App() {  
   const [count, setCount] = useState(0)
-
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      <UserSearch />
+    </div>
+  );
   return (
     <>
       <div>
